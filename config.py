130919dae34668ddec1969ecdfc5ab561b5da19a
Andrew N <34643231+Bopobywek@users.noreply.github.com>
2020-11-20 18:12:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-TOKEN = 'f61fa001ca04c59a4f901f52e03d6dc0a5053ef5206cac2c3f77bdbad14c0e491fc7515ef7e01c3b68d52'
-CONFIRMATION_TOKEN = '91fb99dd'
+TOKEN = 'xxxx'
+CONFIRMATION_TOKEN = 'xxxx'
 YANDEX_DISK_DOWNLOAD_URL = 'https://cloud-api.yandex.net/v1/disk/public/resources/download'
 GOOGLE_DRIVE_DOWNLOAD_URL = 'https://docs.google.com/uc'
-YANDEX_DISK_TOKEN = 'AQAAAAAz_EvcAADLW9yxr1pAXkpcvbbEOhaltlE'
+YANDEX_DISK_TOKEN = 'xxxx'
